1014a9420b0281643c39ba0800e7b195e1fe2dd8
Alexandru Dirig <dirig.alexandru@yahoo.com>
2019-07-26 20:07:14 +0300

    First Commit

---
+++ src/main/java/Entitati/Om.java
@@ -0,0 +1,17 @@
+package Entitati;
+
+import lombok.Data;
+
+@Data
+
+public class Om {
+
+    private int varsta;
+    private String numeOm;
+    private String prenumeOm;
+
+    public void afisareVarsta (Om omulet){
+        System.out.println(omulet.getNumeOm() + " are varsta de: " + omulet.getVarsta()+ " ani.");
+    }
+
+}
---
+++ src/main/java/Executie.java
@@ -0,0 +1,10 @@
+import Entitati.Om;
+
+public class Executie {
+    public static void main(String[] args) {
+        Om x = new Om();
+        x.setNumeOm("Catalin");
+        x.setVarsta(20);
+        x.afisareVarsta(x);
+    }
+}
